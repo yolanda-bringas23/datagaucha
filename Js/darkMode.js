6f1acf67898c818a88c58b02df79aec073eb4345
David-irusta <david.irusta@itscordoba.edu.ar>
2026-09-24 05:38:27 -0300

    Boton atras e unificar

--- Js/darkMode.js
+++ Js/darkMode.js
@@ -2,25 +2,27 @@ const boton = document.getElementById("darkMode")
 let darkMode = localStorage.getItem("darkMode")
 const body = document.getElementsByTagName("body")[0]
 
-if(darkMode == "" || darkMode == 'undefined') mode = "dark"
+if(darkMode == null || darkMode == 'undefined') {
+    darkMode = "dark"
+}
 
 if(darkMode === 'dark'){
-    boton.innerText = "Modo oscuro"
+    boton.innerHTML = '<i class="bi bi-moon-fill"></i> <span>Modo oscuro</span>'
     darkMode = 'light'
     body.classList.remove("light")
 } else{
-    boton.innerText = "Modo claro"
+    boton.innerHTML = '<i class="bi bi-sun-fill"></i> <span>Modo claro</span>'
     darkMode = 'dark'
     body.classList.add("light")
 }
 
 boton.addEventListener("click", (evt) =>{
     if(darkMode === 'dark'){
-        boton.innerText = "Modo claro"
+        boton.innerHTML = '<i class="bi bi-sun-fill"></i> <span>Modo claro</span>'
         darkMode = 'light'
         body.classList.add("light")
     } else{
-        boton.innerText = "Modo oscuro"
+        boton.innerHTML = '<i class="bi bi-moon-fill"></i> <span>Modo oscuro</span>'
         darkMode = 'dark'
         body.classList.remove("light")
     }
